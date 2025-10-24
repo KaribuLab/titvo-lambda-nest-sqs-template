424934bc00d77e01f357f52570ddd63802378ac4
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-10-24 19:09:10 -0300

    feat: Add TITVO configuration parameters for AES key path, parameter table name, and event bus name

--- src/configuration.ts
+++ src/configuration.ts
@@ -2,5 +2,8 @@ export default () => ({
   awsStage: process.env.AWS_STAGE ?? 'prod',
   awsEndpoint: process.env.AWS_ENDPOINT,
   awsRegion: process.env.AWS_REGION,
+  aesKeyPath: process.env.TITVO_AES_KEY_PATH,
+  parameterTableName: process.env.TITVO_PARAMETER_TABLE_NAME,
+  eventBusName: process.env.TITVO_EVENT_BUS_NAME,
   dummy: 'dummy',
 });
